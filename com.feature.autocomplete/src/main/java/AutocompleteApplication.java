71152c59a91a6f29990379af8ac93a34720ff99b
Whitley Bacon <whitley_bacon@homedepot.com>
2017-03-24 15:45:22 -0400

    update application.yaml, pom.xml, AutocompleteApplication java

---
+++ com.feature.autocomplete/src/main/java/AutocompleteApplication.java
@@ -0,0 +1,29 @@
+package com.feature.autocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AutocompleteApplication {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    /**
+     * <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
+     * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
+     */
+    @RequestMapping("/_ah/health")
+    public String healthy() {
+        // Message body required though ignored
+        return "Still surviving.";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldApplication.class, args);
+    }
+}
