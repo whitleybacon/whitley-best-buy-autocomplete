dd1189667b6f5f1d5800bc72dbc29f193f751138
Whitley Bacon <whitley_bacon@homedepot.com>
2017-03-24 16:27:06 -0400

    update hello world message - test

--- com.feature.autocomplete/src/main/java/AutocompleteApplication.java
+++ com.feature.autocomplete/src/main/java/AutocompleteApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutocompleteApplication {
     @RequestMapping("/")
     public String home() {
-        return "Hello World!";
+        return "Whitley's Best Buy Autocomplete Feature";
     }
 
     /**
